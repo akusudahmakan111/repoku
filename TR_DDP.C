2fc95043ecd6fea287b3e6d6d5e14cb6de0e651d
YehezkielDR <yehezkielraharjo5@gmail.com>
2025-11-22 11:29:41 +0700

    - menambahkan bagian hotel

--- TR_DDP.C
+++ TR_DDP.C
@@ -3,6 +3,196 @@
 #include <string.h>
 #include <conio.h>
 #include <windows.h>
+#include <time.h>
+
+// BAGIAN PROGRAM HOTEL
+#define MAX 100
+
+typedef struct {
+    char nama_tamu[50];
+    int nomor_kamar;
+    int lama_menginap;
+    char waktu_pesan[40];
+    char waktu_update[40];
+} Tamu;
+
+Tamu daftar_tamu[MAX];
+int banyaktamu = 0;
+
+void data_waktu(char *buffer){
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    strftime(buffer, 40, "%a %b %d %H:%M:%S %Y", t);
+}
+
+int cari_index(char *cari_nama){
+    for(int i = 0; i < banyaktamu; i++){
+        if(strcmp(daftar_tamu[i].nama_tamu, cari_nama) == 0){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void pause_hotel(){
+    printf("Tekan tombol apapun untuk keluar... \n");
+    getchar();
+    getchar();
+}
+
+void loading_hotel(){
+    system("cls");
+    printf("\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t   Loading...");
+    Sleep(1000);
+    system("cls");
+}
+
+void tambah_tamu(){
+    loading_hotel();
+
+    printf("=== TAMBAH TAMU BARU ===\n");
+    printf("Masukkan nama tamu: ");
+    scanf(" %[^\n]", daftar_tamu[banyaktamu].nama_tamu);
+
+    printf("Masukkan nomor kamar: ");
+    scanf("%d", &daftar_tamu[banyaktamu].nomor_kamar);
+    
+    printf("Masukkan lama menginap: ");
+    scanf("%d", &daftar_tamu[banyaktamu].lama_menginap);
+
+    data_waktu(daftar_tamu[banyaktamu].waktu_pesan);
+    data_waktu(daftar_tamu[banyaktamu].waktu_update);
+
+    banyaktamu++;
+
+    printf("\n");
+    printf("Tamu berhasil ditambahkan!\n");
+    pause_hotel();
+    loading_hotel();
+}
+
+void lihat_tamu(){
+    loading_hotel();
+    printf("=== DAFTAR TAMU HOTEL ===\n");
+    printf("+----+----------------------+-------+---------------+-----------------------------+-----------------------------+\n");
+    printf("| No | Nama Tamu            | Kamar | Lama Menginap | Waktu Pemesanan             | Terakhir Update             |\n");
+    printf("+----+----------------------+-------+---------------+-----------------------------+-----------------------------+\n");
+
+    if (banyaktamu == 0){
+        printf("Tidak ada tamu di hotel.\n");
+    }else{
+        for(int i = 0; i < banyaktamu; i++){
+            printf("| %-2d | %-20s | %-5d | %-13d | %-27s | %-27s |\n",
+                i + 1,
+                daftar_tamu[i].nama_tamu,
+                daftar_tamu[i].nomor_kamar,
+                daftar_tamu[i].lama_menginap,
+                daftar_tamu[i].waktu_pesan,
+                daftar_tamu[i].waktu_update
+            );
+        }
+    }
+
+    printf("+----+----------------------+-------+---------------+-----------------------------+-----------------------------+\n");
+    pause_hotel();
+    loading_hotel();
+}
+
+void ubah_menginap(){
+    loading_hotel();
+    if(banyaktamu == 0){
+        printf("Tidak ada tamu untuk diubah.\n");
+        pause_hotel();
+        return;
+    }
+
+    char cari[50];
+    printf("Masukkan nama tamu yang ingin diubah: ");
+    scanf(" %[^\n]", cari);
+
+    int index = cari_index(cari);
+
+    if(index == -1){
+        printf("Tamu dengan nama %s tidak ditemukan.\n", cari);
+        pause_hotel();
+        return;
+    }
+
+    printf("Lama menginap saat ini: %d\n", daftar_tamu[index].lama_menginap);
+    printf("Masukkan lama menginap baru: ");
+    scanf("%d", &daftar_tamu[index].lama_menginap);
+
+    data_waktu(daftar_tamu[index].waktu_update);
+
+    printf("\n Lama menginap berhasil diubah.\n");
+    pause_hotel();
+    loading_hotel();
+}
+
+void hapus_tamu(){
+    loading_hotel();
+    if(banyaktamu == 0){
+        printf("Tidak ada tamu untuk diubah.\n");
+        pause_hotel();
+        return;
+    }
+
+    char cari[50];
+    printf("Masukkan nama tamu yang ingin Check Out / dihapus : ");
+    scanf(" %[^\n]", cari);
+
+    int index = cari_index(cari);
+
+    if(index == -1){
+        printf("Tamu dengan nama %s tidak ditemukan.\n", cari);
+        pause_hotel();
+        return;
+    }
+
+    for(int i = index; i < banyaktamu - 1; i++){
+        daftar_tamu[i] = daftar_tamu[i + 1];
+    }
+
+    banyaktamu--;
+
+    printf("\n Tamu '%s' berhasil dihapus dari daftar.\n", cari);
+    pause_hotel();
+    loading_hotel();
+}
+
+void hotel(){
+    int menu;
+    
+    do{
+        system("cls");
+        printf("\n\n\n\n\n\n\n\t\t\t\t\t\t==============================\n");
+        printf("\t\t\t\t\t\t    PROGRAM HOTEL SEDERHANA   \n");
+        printf("\t\t\t\t\t\t==============================\n");
+        printf("\t\t\t\t\t\t1. Tambah Tamu\n");
+        printf("\t\t\t\t\t\t2. Lihat Semua Tamu\n");
+        printf("\t\t\t\t\t\t3. Ubah Lama Menginap\n");
+        printf("\t\t\t\t\t\t4. Check-Out / Hapus Tamu\n");
+        printf("\t\t\t\t\t\t5. Kembali\n");
+        printf("\t\t\t\t\t\t==============================\n");
+        printf("\t\t\t\t\t\tPilih menu: ");
+        scanf("%d", &menu);
+
+        switch (menu)
+        {
+        case 1: tambah_tamu(); break;
+        case 2: lihat_tamu(); break;
+        case 3: ubah_menginap(); break;
+        case 4: hapus_tamu(); break;
+        case 5 :
+            printf("Keluar dari program...\n");
+            break;
+        default:
+            printf("Menu tidak valid!\n");
+        }
+        
+    } while (menu != 5);
+    loading_hotel();
+}
 
 void author(){
     system("cls");
@@ -96,6 +286,7 @@ void menu(){
         Sleep(500);
         printf("\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tTunggu sebentar...");
         Sleep(2000);
+        hotel();
         menu();
         break;
     case 6:
